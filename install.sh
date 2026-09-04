08106232693af25696361947a6369b192a7c2a61
SamuGallo_06 <samu.gallicani@gmail.com>
2026-09-04 23:23:41 +0200

    feat: Update installation script and add requirements file - Enhanced install.sh to copy the Noot application from the distribution directory and set executable permissions. - Added requirements.txt to specify necessary Python packages for the project.

--- install.sh
+++ install.sh
@@ -6,24 +6,25 @@ echo "#  NOOT - Non-apple Open-source Operator for iTunes   #"
 echo "#                                                     #"
 echo "#######################################################"
 echo ""
-# Check if the script is run as root
+
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root (e.g., using sudo)"
-  exit 1
+    echo "Please run as root (e.g., using sudo)"
+    exit 1
 fi
 
-echo "Installing dependencies..."
-# Update package list and install dependencies
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Installing system dependencies..."
 apt-get update
 apt-get install -y usbmuxd libusb-1.0-0
 systemctl enable --now usbmuxd
-pip3 install -r requirements.txt
 
 echo "Installing NOOT..."
 mkdir -p /opt/noot
-cp main.py /opt/noot
-cp idevice.py /opt/noot
-cp noot /usr/local/bin/noot
+cp -r "$SCRIPT_DIR/dist/Noot/." /opt/noot/
+chmod +x /opt/noot/Noot
+
+cp "$SCRIPT_DIR/noot" /usr/local/bin/noot
 chmod +x /usr/local/bin/noot
 
 echo "Installation complete. You can now run NOOT using the command 'noot'."
